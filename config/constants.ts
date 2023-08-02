9ac68d5f6538b6e6d1a96b33b4ec89fa6216ed8c
Iniodu Emmanuel <66440588+codetective@users.noreply.github.com>
2023-08-02 06:25:54 +0100

    Update production api

--- config/constants.ts
+++ config/constants.ts
@@ -1,4 +1,4 @@
 export const localToken = '_94hndjfdsklsffkfjnfjjn'
-export const backendUrl = 'http://localhost:8000'
-// export const backendUrl = 'https://educationfundswd.com.ng/api/public'
+// export const backendUrl = 'http://localhost:8000'
+export const backendUrl = 'https://educationfundswd.com.ng/api/public'
 
